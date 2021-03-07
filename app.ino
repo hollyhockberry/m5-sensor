59e11c547f80a1a01b5afcf8664f4a33f804efd3
Inaba <49336378+hollyhockberry@users.noreply.github.com>
2021-03-07 11:30:39 +0900

    Connect to InfluxDB

--- app.ino
+++ app.ino
@@ -2,15 +2,37 @@
 // Copyright (c) 2021 Inaba
 
 #include <M5Atom.h>
+#include <InfluxDbClient.h>
 #include "dhtmeter.h"
+#include "networkutil.h"
+
+#include "settings.h"
 
 namespace {
 const int kInterval = 5 * 1000;
 DhtMeter dht(25, DHT11);
+
+NetworkUtil network;
+InfluxDBClient influxclient;
+Point dataPoint(INFLUXDB_MEASUREMENT);
 }  // namespace
 
 void setup() {
   M5.begin(true, false, false);
+
+  network.begin("", SSID, PSK);
+
+  String host = "http://" + NetworkUtil::resolveAddress(INFLUXDB_HOST)
+                          + ":" + String(INFLUXDB_PORT, DEC);
+  influxclient.setConnectionParamsV1(
+    host.c_str(),
+    INFLUXDB_DB_NAME,
+    INFLUXDB_USER,
+    INFLUXDB_PASSWORD);
+  influxclient.validateConnection();
+
+  dataPoint.addTag("place", INFLUXDB_TAG_PLACE);
+
   dht.begin();
   dht.Fahrenheit(false);
 }
@@ -18,11 +40,13 @@ void setup() {
 void loop() {
   int t = ::millis();
 
-  if (dht.update()) {
-    Serial.println(
-      "Temperature: " + String(dht.Temperature()) +
-      " Humidity: " + String(dht.Humidity()) +
-      " HeatIndex: " + String(dht.HeatIndex()));
+  if (network.update() && dht.update()) {
+    dataPoint.clearFields();
+    dataPoint.addField("temperature", dht.Temperature());
+    dataPoint.addField("humidity", dht.Humidity());
+    dataPoint.addField("heatindex", dht.HeatIndex());
+
+    influxclient.writePoint(dataPoint);
   }
 
   t = ::millis() - t;
